177714393ebe6669a316a1b22d27c7fb32da2e82
zaporter-work <work-github@zackporter.com>
2023-02-24 14:05:02 -0500

    Add etc directory

---
+++ etc/packaging/appimages/package_release_orb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+SELF=$(realpath $0)
+source "$(dirname $SELF)/utils.sh"
+
+if get_version_tag > /dev/null
+then
+	CUR_TAG=$(get_version_tag)
+	BUILD_CHANNEL=stable appimage-builder --recipe orb_grpc_server-`uname -m`.yml
+	BUILD_CHANNEL=$CUR_TAG appimage-builder --recipe orb_grpc_server-`uname -m`.yml
+fi
