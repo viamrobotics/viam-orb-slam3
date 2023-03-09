710034f5193fee476867c5ef587237131a417be3
JohnN193 <92045055+JohnN193@users.noreply.github.com>
2023-03-09 15:53:23 -0500

    [RSDK-2215] remove old api endpoints (#13)

--- viam-orb-slam3/orbslam_server_v1_main.cc
+++ viam-orb-slam3/orbslam_server_v1_main.cc
@@ -185,7 +185,6 @@ int main(int argc, char **argv) {
         slamService.ProcessDataOnline(SLAM.get());
         slamService.StopSaveAtlasAsOsa();
     }
-    // slamService.ProcessDataForTesting(SLAM.get());
 
     slamService.SetSlam(nullptr);
     SLAM->Shutdown();
